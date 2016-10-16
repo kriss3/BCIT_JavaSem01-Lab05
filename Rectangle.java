08dbe7c37b48b12090026f6ebcffa8239471f43e
Kris <kris_s2@outlook.com>
2016-10-16 16:25:32 -0700

    Adding javadoc comments to the Rectangle class: properties and methods

--- Rectangle.java
+++ Rectangle.java
@@ -15,6 +15,11 @@ public class Rectangle
     private double rectangleLengthInCm;
     private double rectangleWidthInCm;
     
+    /**
+     * Parameterless constructor <br>
+     * Increments static counter when new object is created <br>
+     * Assigns default values to Length and Width
+     */
     public Rectangle()
     {
         counter++;
@@ -22,6 +27,15 @@ public class Rectangle
         this.rectangleWidthInCm = DEFAULT_WIDTH;
     }
     
+    
+    /**
+     * Custom constructor taking two params for rectangle Length and Width <br>
+     * Increment class level counter; <br>
+     * Checks passed in parameters and if any of them is less then 0 default values
+     * are assigned to instance variables
+     * @param rectangle length as int;
+     * @param rectangle width as int
+     */
     public Rectangle(int rectangleLengthInCm, int rectangleWidthInCm)
     {
         counter++;
@@ -37,6 +51,14 @@ public class Rectangle
         }
     }
     
+    /**
+     * Custom constructor taking two params for rectangle Length and Width <br>
+     * Increment class level counter; <br>
+     * Checks passed in parameters and if any of them is less then 0 default values
+     * are assigned to instance variables
+     * @param rectangle length as double;
+     * @param rectangle width as double
+     */
     public Rectangle(double rectangleLengthInCm, double rectangleWidthInCm)
     {
         counter++;
@@ -53,32 +75,62 @@ public class Rectangle
     }
     
     //Properties
+    /**
+     * Public getter for RectangleLengthInCm <br>
+     * It does not take any parameters
+     * @return length of rectangle as Double
+     */
     public double getRectangleLengthInCm()
     {
         return this.rectangleLengthInCm;
     }
     
-    public void setrRectangleLengthInCm(double rectangleLengthInCm)
+    /**
+     * Public setter for RectangleLengthInCm <br>
+     * It does return any value
+     * @param new value of rectangle Length in cm as Double
+     */
+    public void setRectangleLengthInCm(double rectangleLengthInCm)
     {
         this.rectangleLengthInCm = (rectangleLengthInCm < 0 ? DEFAULT_LENGTH : rectangleLengthInCm);
     }
     
+    /**
+     * Public getter for RectangleWidthInCm <br>
+     * It does not take any parameters
+     * @return width of rectangle as Double
+     */
     public double getrectangleWidthInCm()
     {
         return this.rectangleWidthInCm;
     }
     
+    /**
+     * Public setter for RectangleWidthInCm <br>
+     * It does return any value
+     * @param new value of rectangle widht in cm as double
+     */
     public void setRectangleWidthInCm(double rectangleWidthInCm)
     {
         this.rectangleWidthInCm = (rectangleWidthInCm < 0 ? DEFAULT_WIDTH : DEFAULT_WIDTH);
     }
     
     //Public methods
+    /**
+     * Public method isSquare takes no parametes and returns flag whether Rectangles
+     * is actually a Square
+     * @return a flag as a Boolean
+     */
     public boolean isSquare()
     {
         return (rectangleLengthInCm == rectangleWidthInCm ? true : false);
     }
     
+    /**
+     * Public method that calculates 
+     * @param format as char
+     * @return area of a rectangle as double
+     */
     public double getArea(char format)
     {
         double result;
@@ -95,20 +147,33 @@ public class Rectangle
         }
     }
     
+    /**
+     * Public method that calculates the perimeteer of a rectangle <br>
+     * It does not take any input parameters
+     * @return returns perimeter of a rectangle as double;
+     */
     public double getPerimeter()
     {
         return ((2 * rectangleLengthInCm) + (2 * rectangleWidthInCm));
     }
     
+    /**
+     * Class level method that return a number of created Rectangle objects
+     * @return counter of all rectangles as int
+     */
     public static int getCount()
     {
         return counter;
     }
     
+    /**
+     * Public method showing details of the rectagled class (its private members) <br>
+     * It does not take any parameters <br>
+     * @return details of rectangle class as String
+     */
     public String toString()
     {
         return String.format("Rectagular Length: " + rectangleLengthInCm + 
                              "Rectagular Widht: " + rectangleWidthInCm);
     }
-    
 }
